f209726008615106c4841743cd807a8cac03df78
Alexander Harry <35274582+AlexHarry17@users.noreply.github.com>
2019-12-08 12:27:58 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
-# Check if running as root
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Must run as root user"
-    exit
-fi
 # Set to breeze theme
 lookandfeeltool -a 'org.kde.breezedark.desktop'
 
 # Update Files
-dnf update -y
+sudo dnf update -y
 
 #Install Brave
-dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-dnf install brave-browser -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser -y
 
 #Install vscode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
-dnf install code -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code -y
 
 # Remove unwanted packages
-dnf remove firefox kmahjongg kpat kmines kruler falkon kmail ktorrent k3b calligra-* -y
+sudo dnf remove firefox kmahjongg kpat kmines kruler falkon kmail ktorrent k3b calligra-* -y
 
 # Install git, redshift
-dnf install git flatpak redshift libreoffice simple-scan plasma-applet-redshift-control -y
+sudo dnf install git flatpak redshift libreoffice simple-scan plasma-applet-redshift-control -y
 
 #Enable Flatpak
 echo '----------Enable Flatpak----------'
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Install Spotify Flatpak
 echo '----------Installing Spotify----------'
-flatpak install flathub com.spotify.Client -y
+sudo flatpak install flathub com.spotify.Client -y
 
 mkdir ~/Desktop/Programs/
 
@@ -56,27 +51,6 @@ rm jetbrains*.tar.gz
 else
 echo 'BAD JETBRAINS CHECKSUM.'
 fi
-
-
-# Get Brother printer driver
-wget 'https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.1-1.gz'
-gunzip *brprinter*.gz
-rm *brprinter*.gz
-chmod +x *brprinter*
-# sudo ./*brprinter* << EOF
-./*brprinter* << EOF
-MFC-7360N
-y
-Y
-Y
-y
-y
-n
-N
-Y
-Y
-\n
-EOF
 
 # Set redshift temperature
 echo "
@@ -102,9 +76,9 @@ export PS1='\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ '
 # Install NVIDIA Drivers
 if [[$(lspci | grep -E "VGA|3D") == *"NVIDIA"*]]; then
 echo 'NVIDIA drivers Found.'
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs
-grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs
+sudo grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 else
 echo 'No NVIDIA drivers found.'
 fi
