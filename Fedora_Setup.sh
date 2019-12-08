44ad41649130fa24836d7c92c5e9ecc51a7289ae
Alexander Harry <35274582+AlexHarry17@users.noreply.github.com>
2019-12-08 01:26:25 -0800

    Update Fedora_Setup.sh

--- Fedora_Setup.sh
+++ Fedora_Setup.sh
@@ -8,9 +8,6 @@ fi
 # Update Files
 dnf update -y
 
-# Install KDE
-dnf install @kde-desktop-environment -y
-
 #Install Brave
 dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
