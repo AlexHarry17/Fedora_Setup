51dcc5eec2249cf017ce9d60d6bc7ba7f3199a22
Alexander Harry <35274582+AlexHarry17@users.noreply.github.com>
2019-12-08 13:08:10 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,31 +4,41 @@
 lookandfeeltool -a 'org.kde.breezedark.desktop'
 
 # Update Files
+echo '---------- Updating packages ----------'
+
 sudo dnf update -y
 
 #Install Brave
+echo '---------- Installing Brave Browser ----------'
+
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
 #Install vscode
+echo '---------- Installing VS Code ----------'
+
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
 sudo dnf install code -y
 
 # Remove unwanted packages
+echo '---------- Removing unwanted packages ----------'
+
 sudo dnf remove firefox kmahjongg kpat kmines kruler falkon kmail ktorrent k3b calligra-* -y
 
 # Install git, redshift
+echo '---------- Installing wanted packages ----------'
+
 sudo dnf install git flatpak redshift libreoffice simple-scan plasma-applet-redshift-control redshift-gtk -y
 
 #Enable Flatpak
-echo '----------Enable Flatpak----------'
+echo '---------- Enable Flatpak ----------'
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Install Spotify Flatpak
-echo '----------Installing Spotify----------'
+echo '---------- Installing Spotify ----------'
 sudo flatpak install flathub com.spotify.Client -y
 
 mkdir ~/Desktop/Programs/
@@ -45,11 +55,12 @@ CHECK="$(sha256sum -c jetbrains*.sha256)"
 echo "$CHECK"
 # Verify Jetbrains toolbox checksum
 if [[ "$(sha256sum -c jetbrains*.sha256)" == *"OK" ]]; then
-echo 'Jetbrains checksum OK.'
+echo '---------- Jetbrains checksum OK ----------'
 tar -xvf jetbrains*.tar.gz
 rm jetbrains*.tar.gz
 else
-echo 'BAD JETBRAINS CHECKSUM.'
+echo '---------- BAD JETBRAINS CHECKSUM ----------'
+exit
 fi
 
 # Set redshift temperature
@@ -74,20 +85,19 @@ export PS1='\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ '
 
 
 # Install NVIDIA Drivers
-if [[$(lspci | grep -E "VGA|3D") == *"NVIDIA"*]]; then
-echo 'NVIDIA drivers Found.'
+echo '----------Checking for NVIDIA Graphics ----------'
+if [[ $(lspci | grep -E "VGA|3D") == *"NVIDIA"* ]]; then
+echo '---------- NVIDIA drivers Found ----------'
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs
 sudo grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 else
-echo 'No NVIDIA drivers found.'
+echo '---------- No NVIDIA drivers found ----------'
 fi
+
 
 
 # Reboot system
 echo '
-----------System will now reboot----------'
-sleep 10
+---------- Installer Finished - Please Reboot ----------'
 
-# sudo reboot
-reboot
