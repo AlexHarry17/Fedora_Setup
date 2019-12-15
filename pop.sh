12424ce3e4a17068c5215884115a79c77bf90614
Alex Harry <alex.harry1@gmail.com>
2019-12-15 00:11:47 -0700

    Cleaned up code with functions

--- pop.sh
+++ pop.sh
@@ -4,52 +4,40 @@ FILE=$(basename $BASH_SOURCE)
 cd ~/.config
 mkdir autostart
 cd
+
 print_good_output () {
-echo -e "\e[36m---------- $1 ----------\e[m
+echo -e "
+\e[36m---------- $1 ----------\e[m
 "
 }
 
 print_error_output () {
-echo -e "\e[1;31m---------- $1 ----------\033[0m
+echo -e "
+\e[1;31m---------- $1 ----------\033[0m
 "
 }
 
-print_good_output "Lets Get Started!"
-print_good_output "Jetbrains Toolbox"
-echo -e '\e[36mCopy the link address of the "direct link" button link from:\e[m' https://www.jetbrains.com/toolbox-app/download/download-thanks.html
-echo -e '\e[36mPaste link address here:\e[m'
-read JETBRAINS_TOOLBOX
+print_no_format_link() {
+  echo -e "\e[36m$1 \e[m" $2  
+}
 
-echo -e '
-\e[36mCopy the link address of the "SHA-256 checksum" button link:\e[m'
-echo -e '\e[36mPaste link address here:\e[m'
-read JETBRAINS_TOOLBOX_CHECKSUM
+print_no_format() {
+  echo -en "\e[36m$1 \e[m" 
+}
 
-print_good_output "Slack"
-
-echo -e '\e[36mCopy the link address of the "Try again" button link from:\e[m' https://slack.com/downloads/instructions/ubuntu
-echo -e '\e[36mPaste link address here:\e[m'
-read SLACK
-
-print_good_output "Github Setup"
-print_good_output "What is your name?:"
-read GITHUB_USER_NAME
-
-print_good_output "What is your email?:"
-read GITHUB_USER_EMAIL
-
-# Ask for brother printer install
+# Brother Printer Setup
+brother_printer_setup () {
 brother_printer=''
-echo -en "\e[36mDo you want to set up a Brother Printer? [y/n] \033[0m"
-
 while true;
 do
+print_no_format "Do you want to set up a Brother Printer? [n/Y]"
 read brother_printer
-if [ $brother_printer = 'y' ]; then
-echo -e '\e[36mVist the following link. Search for your model.  Choose the "Driver Install Tool".  Read and agree to the license. Copy the link address of "If your download does not start automatically, please click here.":\e[m' 'https://support.brother.com/g/b/productsearch.aspx?c=us&lang=en&content=dl'
-echo -e '\e[36mPaste link address here:\e[m'
+if [[ $brother_printer = 'y' || -z $brother_printer ]]; then
+brother_printer='y'
+print_no_format_link 'Vist the following link. Search for your model.  Choose the "Driver Install Tool".  Read and agree to the license. Copy the link address of "If your download does not start automatically, please click here.":' 'https://support.brother.com/g/b/productsearch.aspx?c=us&lang=en&content=dl'
+print_no_format 'Paste link address here:'
 read BROTHER_DRIVER
-echo -e '\e[36mEnter your printer model:\e[m'
+print_no_format 'Enter your printer model:'
 read BROTHER_MODEL
 break
 fi
@@ -59,41 +47,64 @@ else
 print_error_output "Enter 'y' for yes or 'n' for no"
 fi
 done
+}
 
-# Update Files
-print_good_output "Updating packages"
+# Upgrades the packages of the freshly installed systems
+initial_package_upgrade() {
+print_good_output "Upgrading packages"
+sudo apt update && sudo apt upgrade -y --allow-downgrades && sudo apt autoremove -y
+}
 
+# Removes all packages passed into the function
+remove_package() {
+# source for $@: https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script, user Robert Gamble
+for package in "$@"
+do
+print_good_output "Removing package $package"
+sudo apt remove $package -y
+done
+sudo apt autoremove -y 
+}
+
+# Installs all packages passed into the function
+install_package() {
+# source for $@: https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script, user Robert Gamble
+for package in "$@"
+do
 sudo apt update
-sudo apt upgrade -y --allow-downgrades
+print_good_output "Installing package $package"
+sudo apt install $package -y
+done
+}
 
-# Remove unwanted packages
-sudo apt autoremove -y
-print_good_output "Removing unwanted packages"
+# Installs all packages passed into no recommends with the --no-install-recommends flag
+install_package_no_recommends() {
+# source for $@: https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script, user Robert Gamble
+for package in "$@"
+do
+sudo apt update
+print_good_output "Installing package $package"
+sudo apt install $package -y --no-install-recommends
+done
+}
 
-sudo apt --purge remove gedit gnome-weather firefox geary -y 
-
-#Install Brave
+# Install the brave browser
+install_brave_browser() {
 print_good_output "Installing Brave Browser"
-
-sudo apt install apt-transport-https curl -y
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+install_package brave-browser
+}
 
-
-#Install Spotify 
+# Install spotify music player
+install_spotify() {
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client -y
+install_package spotify-client
+}
 
-# Install git, redshift
-print_good_output "Installing wanted packages"
-
-sudo apt update
-sudo apt install xdotool git-lfs deja-dup synaptic gconf2 libdbusmenu-gtk4 scribus libappindicator1 thunderbird gnome-tweaks gnome-shell-extension-ubuntu-dock -y
-
-
+# Install the jetbrains toolbox
+install_jetbrains_toolbox() {
 mkdir ~/Desktop/Programs/
 
 PROGRAM_FOLDER='Desktop/Programs/'
@@ -117,55 +128,76 @@ print_error_output "Jetbrains toolbox will not install."
 fi
 
 rm jetbrains*.tar.gz jetbrains*.sha256
+}
 
-# Setup git user
+# Configures the git config settings
+git_config() {
 git config --global user.name "$GITHUB_USER_NAME"
 git config --global user.email "$GITHUB_USER_EMAIL"
-git config --global color.ui auto
-git config --global color.branch auto
-git config --global color.status auto
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
+}
 
+# Installs additional cuda libraries for nvidia
+install_nvidia() {
 # Install NVIDIA Drivers
 echo -e '\e[36m----------Checking for NVIDIA Graphics ----------\e[m
 '
 if [[ $(lspci | grep -E "VGA|3D") == *"NVIDIA"* ]]; then
 print_good_output "NVIDIA drivers Found"
-sudo apt install system76-cuda-latest system76-cudnn-*.* -y
+install_package system76-cuda-latest system76-cudnn-*.*
 else
 print_good_output "No NVIDIA drivers found"
 fi
+}
 
+# Runs through the brother printer installer
+install_brother_printer() {
 if [ $brother_printer = 'y' ]; then
+print_good_output "Installing Brother Printer"
 wget "$BROTHER_DRIVER"
 gunzip linux-brprinter-installer-*.*.*-*.gz
 sudo bash linux-brprinter-installer-*.*.*-* $BROTHER_MODEL
 rm linux-brprinter-installer* brscan*.deb cupswrapper*.deb mfc*.deb
 fi
+}
 
-# #Install vscode
-
-#Error with download, using snap
-sudo apt update
-sudo apt install snapd
-print_good_output "Installing VS Code"
-
+# Installs VS Code snap package
+install_code() {
 sudo snap install code --classic
+}
 
-# Install slack
+# Installs slack 
+install_slack() {
 sudo apt update
 print_good_output "Installing Slack"
 cd
 wget "$SLACK"
 sudo dpkg -i slack*.deb
 rm slack*.deb
+}
 
-
-print_good_output "Installing Bash Colors"
+# Sets up bash config
+setup_bash(){
+# Clone the bash_it repo
+print_good_output "Setting up bash settings"
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh <<EOF
 N
 EOF
 
+# Add blinking I-beam cursor 
+echo "
+# Add blinking ibeam cursor
+echo -ne '\e[5 q'
+" >> ~/.bashrc
+
+# Sets the desired bash_it theme
+sed -i -e "s/export BASH_IT_THEME='bobby'/export BASH_IT_THEME='powerline-plain'/g" ~/.bashrc
+}
+
+# Opens jetbrains-toolbox to create icon launcher.  Opens code to configure settings.
+open_files() {
 cd $PROGRAM_FOLDER/jetbrains*
 ./jetbrains-tool*
 sleep 2
@@ -174,30 +206,40 @@ cd
 snap run code
 sleep 4
 xdotool windowminimize $(xdotool getactivewindow)
-sudo apt autoremove -y
+}
+
+# A countdown for reboot.
+reboot_countdown() {
 # Reboot system
 for ((countdown=30; countdown>=1; countdown--))
 do
 echo -n -e "\r\e[31m---------- Installer Finished - Rebooting in $countdown seconds ----------\e[m"
     sleep 1
 done
-echo''
-#VSCode Settings
+reboot
+}
+
+# Sets VS code settings to auto save, removes telemetry, and sets the color scheme
+code_settings(){
 echo '{
     "files.autoSave": "afterDelay",
     "telemetry.enableCrashReporter": false,
     "telemetry.enableTelemetry": false,
     "workbench.colorTheme": "Monokai"
 }' >> ~/.config/Code/User/settings.json
+}
 
+# Creates settings via desktop entry
+change_settings() {
 echo '[Desktop Entry]
 Type=Application
 Name=Show Desktop
 Icon=desktop
 Exec=xdotool key --clearmodifiers Super+d' >> ~/.local/share/applications/show-desktop.desktop
+}
 
-# Remove jetbrians-toolbox autostart
-rm ~/.config/autostart/jetbrains*.desktop
+# Edit gsettings/tweak settings
+tweak_extension_settings() {
 # Tweak Settings
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings reset org.gnome.shell.extensions.dash-to-dock dash-max-icon-size
@@ -222,16 +264,50 @@ gsettings set org.gnome.desktop.privacy remove-old-temp-files true
 gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
 gsettings set org.gnome.shell enabled-extensions "['alt-tab-raise-first-window@system76.com', 'always-show-workspaces@system76.com', 'batteryiconfix@kylecorry31.github.io', 'donotdisturb@kylecorry31.github.io', 'pop-shop-details@system76.com', 'pop-suspend-button@system76.com', 'system76-power@system76.com', 'ubuntu-dock@ubuntu.com']"
 gsettings set org.gnome.shell favorite-apps "['brave-browser.desktop', 'thunderbird.desktop', 'spotify.desktop', 'slack.desktop', 'jetbrains-toolbox.desktop', 'code_code.desktop', 'org.gnome.Nautilus.desktop', 'show-desktop.desktop']"
-git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
-sed -i -e "s/export BASH_IT_THEME='bobby'/export BASH_IT_THEME='powerline-plain'/g" ~/.bashrc
+}
 
-echo "
-# Add blinking ibeam cursor
-echo -ne '\e[5 q'
-" >> ~/.bashrc
+print_good_output "Lets Get Started!"
+print_good_output "Jetbrains Toolbox"
+print_no_format_link 'Copy the link address of the "direct link" button link from:' https://www.jetbrains.com/toolbox-app/download/download-thanks.html
+print_no_format 'Paste link address here:'
+read JETBRAINS_TOOLBOX
 
+print_no_format 'Copy the link address of the "SHA-256 checksum" button link.'
+print_no_format 'Paste link address here:'
+read JETBRAINS_TOOLBOX_CHECKSUM
+
+print_good_output "Slack"
+
+print_no_format_link 'Copy the link address of the "Try again" button link from:' https://slack.com/downloads/instructions/ubuntu
+print_no_format 'Paste link address here:'
+read SLACK
+
+print_good_output "Github Setup"
+print_no_format "What is your name?:"
+read GITHUB_USER_NAME
+
+print_no_format "What is your email?:"
+read GITHUB_USER_EMAIL
+
+brother_printer_setup
+initial_package_upgrade
+remove_package gedit gnome-weather firefox geary
+install_package xdotool snapd tensorman apt-transport-https curl git-lfs deja-dup synaptic gconf2 libdbusmenu-gtk4 scribus libappindicator1 thunderbird gnome-tweaks gnome-shell-extension-ubuntu-dock
+install_package_no_recommends tlp tlp-rdw 
+install_brave_browser
+install_spotify
+install_jetbrains_toolbox
+install_nvidia
+install_brother_printer
+install_code
+install_slack
+open_files
+code_settings
+reboot_countdown
+change_settings
+tweak_extension_settings
+reboot_countdown
+#Remove this script file
 cd $DIRECTORY
 rm $FILE
 
-reboot
